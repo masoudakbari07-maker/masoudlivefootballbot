4f7b53bb8a24eb03492d208a21636524de5b5c47
masoudakbari07-maker <masoudakbari07@gmail.com>
2026-06-03 23:19:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -117,5 +117,5 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT, analyze))
 
 print("BOT RUNNING ⚽")
-
-app.run_polling()
+print("STARTING POLLING")
+app.run_polling(drop_pending_updates=True)
